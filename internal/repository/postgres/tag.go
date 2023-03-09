50ed1ee9fa8440c9d1278f92b3cf265bd5f8a8a5
timickb <timickb@yandex.ru>
2023-03-09 19:10:34 +0300

    implemented CreateTag in postgres

--- internal/repository/postgres/tag.go
+++ internal/repository/postgres/tag.go
@@ -1,10 +1,24 @@
 package postgres
 
-import "github.com/timickb/transport-sound/internal/domain"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/timickb/transport-sound/internal/domain"
+)
 
 func (p PqRepository) CreateTag(title string) (*domain.Tag, error) {
-	//TODO implement me
-	panic("implement me")
+	st := `INSERT INTO tags (id, title) VALUES ($1, $2)`
+	id := uuid.NewString()
+
+	_, err := p.db.Exec(st, id, title)
+	if err != nil {
+		return nil, fmt.Errorf("db insertion err: %w", err)
+	}
+
+	return &domain.Tag{
+		Id:    id,
+		Title: title,
+	}, nil
 }
 
 func (p PqRepository) GetTagById(id string) (*domain.Tag, error) {
