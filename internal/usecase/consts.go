91648b9cd12cecb936e0182057e0fb42cfb34fd0
timickb <timickb@yandex.ru>
2023-04-05 19:27:58 +0300

    Improved password validation

---
+++ internal/usecase/consts.go
@@ -0,0 +1,8 @@
+package usecase
+
+const (
+	MinLoginLen    = 3
+	MaxLoginLen    = 20
+	MinPasswordLen = 8
+	MaxPasswordLen = 100
+)
--- internal/usecase/utils/utils_test.go
+++ internal/usecase/utils/utils_test.go
@@ -1,6 +1,10 @@
 package utils
 
-import "testing"
+import (
+	"github.com/timickb/transport-sound/internal/usecase"
+	"strings"
+	"testing"
+)
 
 func TestValidateEmail(t *testing.T) {
 	correct := []string{
@@ -28,6 +32,34 @@ func TestValidateEmail(t *testing.T) {
 	for _, email := range incorrect {
 		if ValidateEmail(email) {
 			t.Fatal("expected false on case", email)
+		}
+	}
+}
+
+func TestValidatePassword(t *testing.T) {
+	correct := []string{
+		"8Ab8Ab8Ab",
+		"A8b_A8b_A8b",
+	}
+	incorrect := []string{
+		"",
+		strings.Repeat("Abc", usecase.MaxPasswordLen/3),
+		strings.Repeat("ABC", usecase.MaxPasswordLen/3),
+		strings.Repeat("abc", usecase.MaxPasswordLen/3),
+		strings.Repeat("123", usecase.MaxPasswordLen/3),
+		strings.Repeat("A8b", usecase.MaxPasswordLen/3+1),
+		strings.Repeat("A8b", usecase.MaxPasswordLen/3)[:usecase.MinPasswordLen-1],
+	}
+
+	for _, pwd := range correct {
+		if !ValidatePassword(pwd) {
+			t.Fatal("expected true on case", pwd)
+		}
+	}
+
+	for _, pwd := range incorrect {
+		if ValidatePassword(pwd) {
+			t.Fatal("expected false on case", pwd)
 		}
 	}
 }
